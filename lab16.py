9d95173857bc985f67d46d7c914b8ba56b01c168
Brittany Mazza <bmazza555@gmail.com>
2015-12-12 11:46:08 -0800

    Add professor's code for part 1

--- lab16.py
+++ lab16.py
@@ -9,3 +9,22 @@ Filename: lab16.py
 Python Version: 2.2.1 (JES 4.3)
 Version: 1
 """
+
+# Part 1 (Written by professor)
+# Slight alteration made to pass "path" in.
+def makePage(path):
+  file = open(path + "/superweb.html", "wt")
+  file.write("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01
+  Transition//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+  <html>
+  <head><title>I made this page with Python!</title>
+  </head>
+  <body>
+  <h1>MY PYTHON PAGE!!!</h1>
+  </body>
+  </html>""")
+
+  file.close()
+
+# Part 2
