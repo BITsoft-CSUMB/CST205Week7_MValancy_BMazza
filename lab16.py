ef3dfa35ee8e2c1ef0d8c6fe763bc50e80c151de
Brittany Mazza <bmazza555@gmail.com>
2015-12-15 22:39:15 -0800

    Add completed lab 16

--- lab16.py
+++ lab16.py
@@ -9,6 +9,8 @@ Filename: lab16.py
 Python Version: 2.2.1 (JES 4.3)
 Version: 1
 """
+import urllib
+
 
 # Part 1 (Written by professor)
 # Slight alteration made to pass "path" in.
@@ -16,7 +18,6 @@ def makePage(path):
   file = open(path + "/superweb.html", "wt")
   file.write("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01
   Transition//EN" "http://www.w3.org/TR/html4/loose.dtd">
-
   <html>
   <head><title>I made this page with Python!</title>
   </head>
@@ -27,4 +28,36 @@ def makePage(path):
 
   file.close()
 
+
 # Part 2
+def aggPage(htmlFilePath):
+  """
+  Aggregate data from path and add the data to the specified html file.
+  """
+  htmlContent = urllib.urlopen('https://www.reddit.com/r/twitter').read()
+  matchString = '<p class=\"title\">'
+  matchStringLen = len(matchString)
+  collectedData = []
+
+  done = False
+  index = 0 # moving index searching for titles
+  while not done:
+    # what we are looking for is inbetween > and </a> at the end after a <p class="title"> is found
+    index = htmlContent.find(matchString, index)
+    if index == -1:
+      done = True
+      continue
+
+    endindex = htmlContent.find("</a>", index)
+    titleStart = htmlContent.rfind(">", index, endindex) + 1
+    match = htmlContent[titleStart:endindex]
+
+    collectedData.append(match)
+    index = endindex
+
+  output = ""
+  for data in collectedData:
+    output += data + "<br>"
+  htmlFile = open(htmlFilePath + "output.html", "a+")
+  htmlFile.write(output)
+  htmlFile.close()
